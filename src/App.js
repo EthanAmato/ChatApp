279f385d27f22e0a36529182b5114c1c7d2570fa
EthanAmato <ethannamato@gmail.com>
2023-01-24 11:41:13 -0500

    completed working prototype of chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useRef } from 'react';
+import { io } from 'socket.io-client';
+import Chat from './Chat';
 
+
+const socket = io.connect("http://localhost:3001")
 function App() {
+
+  const [isJoinedRoom, setIsJoinedRoom] = useState(false)
+  const usernameRef = useRef();
+  const roomRef = useRef();
+
+  const joinRoom = () => {
+    if (usernameRef.current.value !== "" && roomRef.current.value !== "") {
+      setIsJoinedRoom(true);
+      console.log("in")
+      socket.emit("join_room", roomRef.current.value) // emit an event to the backend with event id "join_room" 
+      // picked up in backend by socket.on("join_room")
+
+    }
+    else {
+
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isJoinedRoom && <div className='joinChatWrapper'>
+        <h3>Join Chat</h3>
+        <div className='joinChatInputs'>
+          <label>Username</label>
+          <input type="text" placeholder="John Smith" ref={usernameRef} />
+          <label>Room ID</label>
+          <input type="text" placeholder="Room ID" ref={roomRef} />
+          <button onClick={joinRoom}>Join</button>
+        </div>
+      </div>}
+      {
+        isJoinedRoom &&
+        <div className='joinChatWrapper chatting'>
+          <Chat
+            socket={socket}
+            username={usernameRef.current.value}
+            room={roomRef.current.value}
+          />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,79 @@
+import React, { useRef, useEffect, useState } from 'react'
+
+function Chat({ socket, username, room }) {
+    let messageRef = useRef();
+    let mostRecentMessageRef = useRef();
+    const [messageLog, setMessageLog] = useState([]);
+
+    const sendMessage = async () => {
+        if (messageRef.current.value !== "") {
+            //contains all message data (room, user, id, date)
+            let currentTime = new Date(Date.now());
+            const messageData = {
+                room: room,
+                author: username,
+                message: messageRef.current.value,
+                displayTime: currentTime.getHours() + ":" + currentTime.getMinutes(), //e.g. 12:45,
+                timeId: currentTime
+            }
+
+            await socket.emit("send_message", messageData);
+            setMessageLog((list) => [...list, messageData]);
+            messageRef.current.value = "" //reset message
+            mostRecentMessageRef.current?.scrollIntoView({behavior: "smooth"})
+        }
+    }
+
+    const handleEnter = (e) => {
+        if(e.key==="Enter") sendMessage()
+        
+    }
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            setMessageLog((list) => [...list, data]);
+            mostRecentMessageRef.current?.scrollIntoView({behavior: "smooth"})
+        });
+    }, [socket]);
+
+    return (
+        <>
+            <div className='chat-wrapper'>
+                <div className='chat-header'>
+                    <p>Live Chat</p>
+                </div>
+                <div className='chat-body'>
+                    {messageLog.map((chat, i) => {
+                        return (
+                            <div
+                                id={username === chat.author ? "currentUserChat" : "otherUserchat"}
+                                key={`${chat.timeId}+${chat.author}+${chat.room}+${i}`}
+                                className='message'
+                            >
+                                <div className='message-content'>
+                                    <p>{chat.message}</p>
+                                </div>
+                                <div className='message-meta'>
+                                    <span>{chat.displayTime} {chat.author}</span>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <div ref={mostRecentMessageRef}></div>
+                </div>
+                <div className='chat-footer'>
+                    <div className='chat-input-group'>
+                        <input 
+                            type="text" 
+                            ref={messageRef} 
+                            onKeyDown={handleEnter}
+                        />
+                        <button onClick={sendMessage}>&#9658;</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Chat
